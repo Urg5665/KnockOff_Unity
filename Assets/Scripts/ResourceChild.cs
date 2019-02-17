3744a059d508360a07b2428f10239bd36b07448e
Mark <ms4339@drexel.edu>
2019-02-17 16:34:08 -0500

    Resource Node Ramdomization

---
+++ Assets/Scripts/ResourceChild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceChild : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Card")
+        {
+
+            Debug.Log("Crystal HitByCard");
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/ResourceNode.cs
+++ Assets/Scripts/ResourceNode.cs
@@ -4,31 +4,40 @@ using UnityEngine;
 
 public class ResourceNode : MonoBehaviour
 {
+    public GameObject player1;
+    public GameObject player2;
+
+    
+
     public GameObject[] resObjects;
     public GameObject currentSpawn;
 
-    public int i = 10;
+    public int resIndex;
+
+    public int i = 100;
+
+    private void Awake()
+    {
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+        resIndex = Mathf.RoundToInt(Random.Range(0, 2));
+        currentSpawn = Instantiate(resObjects[resIndex], this.transform);
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentSpawn == null)
+        {
+            resIndex = Mathf.RoundToInt(Random.Range(0, 2));
+            i = 0;
+        }
+
         if (i == 100)
         {
-            //currentSpawn = Instantiate(resObjects[Mathf.RoundToInt(Random.Range(0, 1))], this.transform);
+            currentSpawn = Instantiate(resObjects[resIndex], this.transform);
         }
         i++;
-
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Card")
-        {
-            Destroy(currentSpawn.gameObject);
-            i = 0;
-            Debug.Log("HitByCard");
-        }
     }
 }
