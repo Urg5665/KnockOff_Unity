9150d9b074a9ef396074439efe6ab827a5c1050a
Mark <ms4339@drexel.edu>
2019-02-20 23:17:40 -0500

    OnPlayerUI

---
+++ Assets/Scripts/OnPlayerUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnPlayerUI : MonoBehaviour
+{
+
+
+    public Image northImage;
+    public Image eastImage;
+    public Image southImage;
+    public Image westImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        northImage.alphaHitTestMinimumThreshold = 0.99f;
+        eastImage.alphaHitTestMinimumThreshold = 0.99f;
+        southImage.alphaHitTestMinimumThreshold = 0.99f;
+        westImage.alphaHitTestMinimumThreshold = 0.99f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
