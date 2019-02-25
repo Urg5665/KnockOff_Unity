fc505074e992eb54d3374e0ad3bdf7df2ca59432
Mark <ms4339@drexel.edu>
2019-02-24 22:19:33 -0500

    AOE is Now A Cone ( Sorry All)

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -58,7 +58,6 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
 
-
         if (Input.GetKey(KeyCode.Alpha1)) // Press 1 and 2 to speed or slow game, Degbugging
         {
             Time.timeScale += 0.1f;
@@ -176,7 +175,158 @@ public class PlayerControl : MonoBehaviour
         }
         if (spellSecondary[spellSelected] == "AOE")
         {
+            for (int i = 0; i < 5; i++)
+            {
+                newSpellAOE[i] = Instantiate(spellProjectile[0], this.transform.position, spellProjectile[0].transform.rotation);
+                newSpellAOE[i].transform.position = new Vector3(newSpellAOE[i].transform.position.x, newSpellAOE[i].transform.position.y - .25f, newSpellAOE[i].transform.position.z);
+                newSpellAOE[i].GetComponent<FireBallThrow>().spellNum = spellSelected;
+                newSpellAOE[i].GetComponent<FireBallThrow>().maxRange = 20;
+                // I got Really Really Fucking Lazy and Hard Coded the Draw Cricle about point function to make this work. 
+                //Im ashamed of the following code and wil fix when i figrue out abetter draw circle - Mark
+                if (i == 0)
+                {
+                    AOEpoint.position = player1Aim.transform.position;
+                }
+                if (spellSelected == 0 || spellSelected == 2)
+                {
+                    if (i == 1)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x + 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 2)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x + 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 3)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x - 9, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 4)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x - 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                }
+                if (spellSelected == 1 || spellSelected == 3)
+                {
+                    if (i == 1)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z + 3);
+                    }
+                    if (i == 2)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z + 3);
+                    }
+                    if (i == 3)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z - 9);
+                    }
+                    if (i == 4)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z - 3);
+                    }
+                }
+
+                newSpellAOE[i].GetComponent<FireBallThrow>().transform.LookAt(AOEpoint);
+            }
+            canCast[spellSelected] = false;
+        }
+        if (spellSecondary[spellSelected] == "Range")
+        {
+            newSpell = Instantiate(spellProjectile[0], this.transform.position, spellProjectile[0].transform.rotation);
+            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
+            newSpell.GetComponent<FireBallThrow>().spellNum = spellSelected;
+            newSpell.GetComponent<FireBallThrow>().maxRange = 75;
+            canCast[spellSelected] = false;
+        }
+
+
+
+
+    }
+    private void WindKnockback()
+    {
+
+        if (spellSecondary[spellSelected] == "")
+        {
+            newSpell = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[1].transform.rotation);
+            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
+            newSpell.GetComponent<WindWaveThrow>().spellNum = spellSelected;
+            //Debug.Log("WindWave" + (spellSelected + 1) + " Thrown");
+            canCast[spellSelected] = false;
+            newSpell.GetComponent<WindWaveThrow>().maxRange = 25;
+        }
+        else if (spellSecondary[spellSelected] == "AOE")
+        {
             for (int i = 0; i < 8; i++)
+            {
+                newSpellAOE[i] = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[0].transform.rotation);
+                newSpellAOE[i].transform.position = new Vector3(newSpellAOE[i].transform.position.x, newSpellAOE[i].transform.position.y - .25f, newSpellAOE[i].transform.position.z);
+                newSpellAOE[i].GetComponent<WindWaveThrow>().spellNum = spellSelected;
+                newSpellAOE[i].GetComponent<WindWaveThrow>().maxRange = 25;
+                // I got Really Really Fucking Lazy and Hard Coded the Draw Cricle about point function to make this work. 
+                //Im ashamed of the following code and wil fix when i figrue out abetter draw circle - Mark
+                if (i == 0)
+                {
+                    AOEpoint.position = player1Aim.transform.position;
+                }
+                if (spellSelected == 0 || spellSelected == 2)
+                {
+                    if (i == 1)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x + 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 2)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x + 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 3)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x - 9, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                    if (i == 4)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x - 3, this.transform.position.y, AOEpoint.transform.position.z);
+                    }
+                }
+                if (spellSelected == 1 || spellSelected == 3)
+                {
+                    if (i == 1)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z + 3);
+                    }
+                    if (i == 2)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z + 3);
+                    }
+                    if (i == 3)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z - 9);
+                    }
+                    if (i == 4)
+                    {
+                        AOEpoint.position = new Vector3(AOEpoint.transform.position.x, this.transform.position.y, AOEpoint.transform.position.z - 3);
+                    }
+
+                }
+                newSpellAOE[i].GetComponent<WindWaveThrow>().transform.LookAt(AOEpoint);
+            }
+            canCast[spellSelected] = false;
+        }
+        else if (spellSecondary[spellSelected] == "Range")
+        {
+            newSpell = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[1].transform.rotation);
+            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
+            newSpell.GetComponent<WindWaveThrow>().spellNum = spellSelected;
+            newSpell.GetComponent<WindWaveThrow>().maxRange = 75;
+            canCast[spellSelected] = false;
+        }
+
+    }
+
+
+
+}
+/* for (int i = 0; i < 8; i++)
             {
                 newSpellAOE[i] = Instantiate(spellProjectile[0], this.transform.position, spellProjectile[0].transform.rotation);
                 newSpellAOE[i].transform.position = new Vector3(newSpellAOE[i].transform.position.x, newSpellAOE[i].transform.position.y - .25f, newSpellAOE[i].transform.position.z);
@@ -217,91 +367,6 @@ public class PlayerControl : MonoBehaviour
                 }
                 newSpellAOE[i].GetComponent<FireBallThrow>().transform.LookAt(AOEpoint); 
             }
-        }
-        if (spellSecondary[spellSelected] == "Range")
-        {
-            newSpell = Instantiate(spellProjectile[0], this.transform.position, spellProjectile[0].transform.rotation);
-            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
-            newSpell.GetComponent<FireBallThrow>().spellNum = spellSelected;
-            newSpell.GetComponent<FireBallThrow>().maxRange = 50;
-            canCast[spellSelected] = false;
-        }
-
-
-
-
-    }
-    private void WindKnockback()
-    {
-
-        if (spellSecondary[spellSelected] == "")
-        {
-            newSpell = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[1].transform.rotation);
-            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
-            newSpell.GetComponent<WindWaveThrow>().spellNum = spellSelected;
-            //Debug.Log("WindWave" + (spellSelected + 1) + " Thrown");
-            canCast[spellSelected] = false;
-            newSpell.GetComponent<WindWaveThrow>().maxRange = 25;
-        }
-        else if (spellSecondary[spellSelected] == "AOE")
-        {
-            for (int i = 0; i < 8; i++)
-            {
-                newSpellAOE[i] = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[0].transform.rotation);
-                newSpellAOE[i].transform.position = new Vector3(newSpellAOE[i].transform.position.x, newSpellAOE[i].transform.position.y - .25f, newSpellAOE[i].transform.position.z);
-                newSpellAOE[i].GetComponent<WindWaveThrow>().spellNum = spellSelected;
-                // I got Really Really Fucking Lazy and Hard Coded the Draw Cricle about point function to make this work. 
-                //Im ashamed of the following code and wil fix when i figrue out abetter draw circle - Mark
-                if (i == 0)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x + 10, this.transform.position.y, this.transform.position.z);
-                }
-                if (i == 1)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x + 7, this.transform.position.y, this.transform.position.z + 7);
-                }
-                if (i == 2)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 10);
-                }
-                if (i == 3)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x - 7, this.transform.position.y, this.transform.position.z + 7);
-                }
-                if (i == 4)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x - 10, this.transform.position.y, this.transform.position.z);
-                }
-                if (i == 5)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x - 7, this.transform.position.y, this.transform.position.z - 7);
-                }
-                if (i == 6)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z - 10);
-                }
-                if (i == 7)
-                {
-                    AOEpoint.position = new Vector3(this.transform.position.x + 7, this.transform.position.y, this.transform.position.z - 7);
-                }
-                newSpellAOE[i].GetComponent<WindWaveThrow>().transform.LookAt(AOEpoint);
-            }
-        }
-        else if (spellSecondary[spellSelected] == "Range")
-        {
-            newSpell = Instantiate(spellProjectile[1], this.transform.position, spellProjectile[1].transform.rotation);
-            newSpell.transform.position = new Vector3(newSpell.transform.position.x, newSpell.transform.position.y - .25f, newSpell.transform.position.z);
-            newSpell.GetComponent<WindWaveThrow>().spellNum = spellSelected;
-            newSpell.GetComponent<WindWaveThrow>().maxRange = 50;
-            canCast[spellSelected] = false;
-        }
-
-    }
-
-
-
-}
-/*
  * 
  * 
  *  for(int i = 0; i > 8; i++)
