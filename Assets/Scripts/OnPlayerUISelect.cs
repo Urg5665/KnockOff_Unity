05309d1d6b9753902d5c43e89f03b5d665b77093
Mark <ms4339@drexel.edu>
2019-02-26 02:41:21 -0500

    Player Icon On Ui for Secondary

--- Assets/Scripts/OnPlayerUISelect.cs
+++ Assets/Scripts/OnPlayerUISelect.cs
@@ -18,23 +18,51 @@ public class OnPlayerUISelect : MonoBehaviour
     public Sprite cyan;
     public Sprite blue;
 
+    public GameObject childIcon;
+
+    public Sprite cone;
+    public Sprite line;
+    public Sprite dash;
+
     private void Update()
     {
         if (player1.GetComponent<PlayerControl>().spellPrimary[spellNumber] == "Fire")
         {
             but.image.sprite = red;
+            childIcon.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
         }
         if (player1.GetComponent<PlayerControl>().spellPrimary[spellNumber] == "Wind")
         {
             but.image.sprite = cyan;
+            //67, 215, 255, 255
+            childIcon.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
         }
         if (player1.GetComponent<PlayerControl>().spellPrimary[spellNumber] == "Water")
         {
             but.image.sprite = blue;
+            childIcon.GetComponent<Image>().color = new Color32(0, 0, 255, 255);
         }
         if (player1.GetComponent<PlayerControl>().spellPrimary[spellNumber] == "")
         {
             but.image.sprite = white;
+        }
+
+        if (player1.GetComponent<PlayerControl>().spellSecondary[spellNumber] == "AOE")
+        {
+            childIcon.GetComponent<Image>().sprite = cone;
+        }
+        if (player1.GetComponent<PlayerControl>().spellSecondary[spellNumber] == "Range")
+        {
+            childIcon.GetComponent<Image>().sprite = line;
+        }
+        if (player1.GetComponent<PlayerControl>().spellSecondary[spellNumber] == "Dash")
+        {
+            childIcon.GetComponent<Image>().sprite = dash;
+        }
+        if (player1.GetComponent<PlayerControl>().spellSecondary[spellNumber] == "")
+        {
+            childIcon.GetComponent<Image>().sprite = null;
+            childIcon.GetComponent<Image>().color = new Color32(255,255,255,0);
         }
 
     }       
