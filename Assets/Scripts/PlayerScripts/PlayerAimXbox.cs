810fb88fe7b61f4ab3e07dd56b9c251370cafd4d
Mark <ms4339@drexel.edu>
2019-02-27 08:55:03 -0500

    Fxi Disiapeahring off screen and respawn marker

--- Assets/Scripts/PlayerScripts/PlayerAimXbox.cs
+++ Assets/Scripts/PlayerScripts/PlayerAimXbox.cs
@@ -8,6 +8,9 @@ public class PlayerAimXbox : MonoBehaviour
     public GameObject player;
     public PlayerControlXbox playerControlXbox;
 
+    public float xDif;
+    public float zDif;
+
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -18,13 +21,30 @@ public class PlayerAimXbox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        xDif = Mathf.Abs(this.transform.position.x - parent.position.x);
+        zDif = Mathf.Abs(this.transform.position.z - parent.position.z);
+
+        if ( xDif > 50)
+        {
+            this.transform.position = new Vector3(parent.position.x, transform.position.y, parent.position.z);
+        }
+
+        if (zDif > 50)
+        {
+            this.transform.position = new Vector3(parent.position.x, transform.position.y, parent.position.z);
+        }
+
         if (Input.GetAxis("HorAim") > 0)
             transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
         if (Input.GetAxis("HorAim") < 0)
             transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
         if (Input.GetAxis("VerAim") < 0)
-            transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
         if (Input.GetAxis("VerAim") > 0)
-            transform.Translate(Vector3.back * Time.deltaTime * speed, Space.World);
+                transform.Translate(Vector3.back * Time.deltaTime * speed, Space.World);
+        
+
+
+
     }
 }
