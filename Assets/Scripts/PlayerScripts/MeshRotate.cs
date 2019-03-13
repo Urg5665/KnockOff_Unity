8092bcee984d191c780e069ffaa7138e5101f0eb
Mark <ms4339@drexel.edu>
2019-03-13 00:20:50 -0400

    PLayer 2 Rotate

--- Assets/Scripts/PlayerScripts/MeshRotate.cs
+++ Assets/Scripts/PlayerScripts/MeshRotate.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class MeshRotate : MonoBehaviour
 {
+    public int playerBelong;
     public GameObject playerAim;
 
-    // Update is called once per frame
     void Update()
     {
+        if (playerBelong == 1)
+        {
+            transform.LookAt(playerAim.transform.position);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 45, transform.eulerAngles.z);
+        }
+        else if (playerBelong == 2)
+        {
+            transform.LookAt(playerAim.transform.position);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 45, transform.eulerAngles.z);
+        }
 
-        transform.LookAt(playerAim.transform.position);
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 45, transform.eulerAngles.z);
 
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerAimXbox.cs
+++ Assets/Scripts/PlayerScripts/PlayerAimXbox.cs
@@ -70,11 +70,12 @@ public class PlayerAimXbox : MonoBehaviour
         if ((Input.GetAxis("HorAim") != 0 || (Input.GetAxis("VerAim") != 0)))
         {
             Vector3 privousPos = this.transform.position;
-            Vector3 nextPos = new Vector3(parent.transform.position.x + (Input.GetAxis("HorAim") * 10), 3, parent.transform.position.z - (Input.GetAxis("VerAim") * 10));
+            Vector3 nextPos = new Vector3(parent.transform.position.x + (Input.GetAxis("HorAim") * 10), parent.transform.position.y, parent.transform.position.z - (Input.GetAxis("VerAim") * 10));
             transform.position = Vector3.Lerp(privousPos, nextPos, Time.deltaTime * 20);
-            transform.position = new Vector3(this.transform.position.x, 3, transform.position.z);
+
 
         }
+        transform.position = new Vector3(this.transform.position.x, parent.transform.position.y, transform.position.z);
 
     }
 }
