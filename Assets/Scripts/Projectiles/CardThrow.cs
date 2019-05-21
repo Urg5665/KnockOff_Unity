54a7b2b5e95c0191d9a80024f4720ac2cf57c485
Mark <39248834+Urg5665@users.noreply.github.com>
2019-05-21 14:55:56 -0400

    Fun with Lighting, Circle Sun

--- Assets/Scripts/Projectiles/CardThrow.cs
+++ Assets/Scripts/Projectiles/CardThrow.cs
@@ -162,7 +162,7 @@ public class CardThrow : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, this.transform.localRotation.y, 0);
             cardCollider.isTrigger = true;
         }
-        if (toPlayer && rangeCounter > maxRange * 1.5f && Vector3.Distance(this.transform.position, player.transform.position) < 1)  // Fixing Card on player bug
+        if (toPlayer && rangeCounter > maxRange * 0.5f && Vector3.Distance(this.transform.position, player.transform.position) < 1)  // Fixing Card on player bug
         {
             //Debug.Log("bugFixed cardeffect given");
             transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed, Space.World);
