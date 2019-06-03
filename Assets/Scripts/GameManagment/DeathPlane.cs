974f7da3c7afa1f99488f609994d297bd577a515
Mark <39248834+Urg5665@users.noreply.github.com>
2019-06-03 14:57:49 -0400

    Adding Clock, background and ui death animation

--- Assets/Scripts/GameManagment/DeathPlane.cs
+++ Assets/Scripts/GameManagment/DeathPlane.cs
@@ -25,11 +25,20 @@ public class DeathPlane : MonoBehaviour
 
     public AudioSource audioSource;
 
+    public bool uiAnimP1;
+    public bool uiAnimP2;
+    public int uiP1Time;
+    public int uiP2Time;
+
     private void Awake()
     {
         audioSource = this.GetComponent<AudioSource>();
         player1Score = 3;
         player2Score = 3;
+        uiAnimP1 = false;
+        uiAnimP2 = false;
+        uiP1Time = 0;
+        uiP2Time = 0;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -44,8 +53,8 @@ public class DeathPlane : MonoBehaviour
             player2.GetComponent<Rigidbody>().velocity = Vector3.zero;
             StartCoroutine(cameraMove.Shake(.3f, 1f));
             audioSource.Play();
-            Destroy(player1Lives[player1Score]);
-            print("Collider Kill");
+            uiAnimP1 = true;
+            //print("Collider Kill");
 
         }
         if (collision.gameObject.tag == "Player2")
@@ -58,8 +67,9 @@ public class DeathPlane : MonoBehaviour
             player1.GetComponent<Rigidbody>().velocity = Vector3.zero;
             StartCoroutine(cameraMove.Shake(.3f, 1f));
             audioSource.Play();
-            Destroy(player2Lives[player2Score]);
-            print("Collider Kill");
+            //Destroy(player2Lives[player2Score]);
+            uiAnimP2 = true;
+            //print("Collider Kill");
         }
         if (collision.gameObject.tag == "Dummy")
         {
@@ -76,6 +86,47 @@ public class DeathPlane : MonoBehaviour
     {
         player1ScoreText.text = player1Score.ToString();
         player2ScoreText.text = player2Score.ToString();
+        if (uiAnimP1)
+        {
+            uiP1Time++;
+            if (uiP1Time < 100)
+            {
+                if (uiP1Time <= 10)
+                {
+                    player1Lives[player1Score].transform.eulerAngles += new Vector3(0.00f, 0.00f, 4.5f);
+                }
+                player1Lives[player1Score].transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            }
+
+            if (uiP1Time > 100)
+            {
+                uiAnimP1 = false;
+                Destroy(player1Lives[player1Score].gameObject);
+                uiP1Time = 0; 
+            }
+        }
+        if (uiAnimP2)
+        {
+            uiP2Time++;
+            if(uiP2Time < 100)
+            {
+                if (uiP2Time <= 10)
+                {
+                    player2Lives[player2Score].transform.eulerAngles += new Vector3(0.00f, 0.00f, 4.5f);
+                }
+                player2Lives[player2Score].transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
+            }
+
+            if (uiP2Time > 100)
+            {
+                uiAnimP2 = false;
+                Destroy(player2Lives[player2Score]);
+                uiP2Time = 0;
+                
+            }
+        }
+
+
         if ( player1.transform.position.y < -17)
         {
             player1Score--;
@@ -86,8 +137,9 @@ public class DeathPlane : MonoBehaviour
             player2.GetComponent<Rigidbody>().velocity = Vector3.zero;
             StartCoroutine(cameraMove.Shake(.3f, 1f));
             audioSource.Play();
-            Destroy(player1Lives[player1Score]);
-            print("Height Kill");
+            //Destroy(player1Lives[player1Score]);
+            uiAnimP1 = true;
+            //print("Height Kill");
         }
         if (player2.transform.position.y < -17)
         {
@@ -99,8 +151,9 @@ public class DeathPlane : MonoBehaviour
             player1.GetComponent<Rigidbody>().velocity = Vector3.zero;
             StartCoroutine(cameraMove.Shake(.3f, 1f));
             audioSource.Play();
-            Destroy(player2Lives[player2Score]);
-            print("Height Kill");
+            //Destroy(player2Lives[player2Score]);
+            uiAnimP2 = true;
+            //print("Height Kill");
         }
     }
 }
---
+++ Assets/Scripts/GameManagment/PrintClock.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrintClock : MonoBehaviour
+{
+    public Text clockText;
+    public int sec;
+    public int min;
+    public int milSec;
+    private void Start()
+    {
+        min = 0;
+        sec = 01;
+        milSec = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        milSec += 1;
+        if (sec < 10)
+        {
+            clockText.text = min + ":0" + sec;
+        }
+        else
+        {
+            clockText.text = min + ":" + sec;
+        }
+        if (milSec == 60)
+        {
+            sec++;
+            milSec = 0;
+        }
+        if (sec == 60)
+        {
+            min++;
+            sec = 0;
+        }
+
+    }
+}
