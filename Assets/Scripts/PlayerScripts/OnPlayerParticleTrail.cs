7406a0a0d918ff2d829415cc46607c5772164d0b
Mark <39248834+Urg5665@users.noreply.github.com>
2019-06-24 09:44:00 -0400

    I never used underscores, why did i put an underscore in the class name lol

--- Assets/Scripts/PlayerScripts/OnPlayerParticleTrail.cs
+++ Assets/Scripts/PlayerScripts/OnPlayerParticleTrail.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnPlayer_ParticleTrail : MonoBehaviour
+public class OnPlayerParticleTrail : MonoBehaviour
 {
     // Should be labled as NeedsImplmentation
-    // Ment to be a script where the player changes a trailing particle based on what
+    // Meant to be a script where the player changes a trailing particle based on what
     // they currnetly have slected in a particular direction
 
     public GameObject[] particleTypes;
-    public
 
     void Start()
     {
