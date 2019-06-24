bb232dad65d44ce3871e7f4612dfebff2157877b
Mark <39248834+Urg5665@users.noreply.github.com>
2019-06-24 09:36:52 -0400

    Renaming yet to be implmented Script

---
+++ Assets/Scripts/PlayerScripts/OnPlayer_ParticleTrail.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPlayer_ParticleTrail : MonoBehaviour
+{
+    // Should be labled as NeedsImplmentation
+    // Ment to be a script where the player changes a trailing particle based on what
+    // they currnetly have slected in a particular direction
+
+    public GameObject[] particleTypes;
+    public
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
